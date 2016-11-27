0d5bb25259e8047cd75bdf06840f51973551ebff
talgatm <talgatm07@gmail.com>
2016-11-27 22:10:25 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 
 int main(int argc, char *argv[]) {
     Mat image, out;
-    image = imread("/home/kb1prb13/LOCAL/IG_1-14/2016-2017/Image/Intensity/lena.png", CV_LOAD_IMAGE_GRAYSCALE);
+    image = imread(""/home/tm/lena.jpg"", CV_LOAD_IMAGE_GRAYSCALE);
     if(!image.data)return -1;
     image.covertTo(out, CV_8U)
     for (int y = 0; y < out.cols; y++) {
