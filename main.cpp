cef98b7862c6f35e93deee6df27b7d949b570238
talgatm <talgatm07@gmail.com>
2016-11-27 22:11:41 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ int main(int argc, char *argv[]) {
     image = imread(""/home/tm/lena.jpg"", CV_LOAD_IMAGE_GRAYSCALE);
     if(!image.data)return -1;
     image.covertTo(out, CV_8U)
+        
     for (int y = 0; y < out.cols; y++) {
         for (int x = 0; x < out.rows; x++) {
             out.at<uchar>(y, x) /= 2;
         }
+        threshold (out,out,64,128,THRESH_BINARY);
     }
 
     return 0;
