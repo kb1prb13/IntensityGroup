15db1646e067dd63446194f4fb6c7731d9b66a87
kb1prb13 <dastan.namazov.96@gmail.com>
2016-11-27 22:59:55 +0600

    Head

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@ using namespace cv;
 int main(int argc, char *argv[]) {
     Mat image, out;
     image = imread("/home/kb1prb13/LOCAL/IG_1-14/2016-2017/Image/Intensity/lena.png", CV_LOAD_IMAGE_GRAYSCALE);
-    if (!image.data) return -1;
-
-    image.convertTo(out, CV_8U);
 
     for (int y = 0; y < out.cols; y++) {
         for (int x = 0; x < out.rows; x++) {
